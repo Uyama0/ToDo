6843bd2f06171e756555592bcddd7d00346e8e17
Ruslan <axelezgames@gmail.com>
2023-04-26 00:42:38 +0500

    add: task list

---
+++ script.js
@@ -0,0 +1,9 @@
+const taskInput = document.getElementsByClassName("taskInput");
+const taskList = document.getElementsByClassName("taskList");
+
+function addYask() {
+  if (taskInput.value == "") {
+    alert("Task can't be empty!");
+  } else {
+  }
+}
