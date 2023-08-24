d006bbeda525e6db5e8e054ebb765e8248cf6ff2
Ruslan <axelezgames@gmail.com>
2023-08-24 23:39:05 +0500

    clean for v2 todo

--- script.js
+++
@@ -1,9 +0,0 @@
-const taskInput = document.getElementById("taskInput");
-const tasks = document.getElementById("tasks");
-
-function addTask() {
-  let newTask = document.createElement("div");
-  newTask.classList.add("taskList");
-  newTask.innerHTML = taskInput.value;
-  tasks.appendChild(newTask);
-}
